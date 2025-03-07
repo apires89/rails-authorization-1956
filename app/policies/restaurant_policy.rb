05917ef9e2016b2d4ff5c791e158f782b0563a5b
andre <andrecabralpires@gmail.com>
2025-03-07 11:40:06 +0000

    pundit

---
+++ app/policies/restaurant_policy.rb
@@ -0,0 +1,34 @@
+class RestaurantPolicy < ApplicationPolicy
+  # NOTE: Up to Pundit v2.3.1, the inheritance was declared as
+  # `Scope < Scope` rather than `Scope < ApplicationPolicy::Scope`.
+  # In most cases the behavior will be identical, but if updating existing
+  # code, beware of possible changes to the ancestors:
+  # https://gist.github.com/Burgestrand/4b4bc22f31c8a95c425fc0e30d7ef1f5
+
+  class Scope < ApplicationPolicy::Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    # if current_user is the same as the restaurant.user
+    record.user == user || user.admin == true
+
+    # record: the restaurant passed to the `authorize` method in controller
+    # user: the `current_user` signed in with Devise
+  end
+
+  def destroy?
+    record.user == user || user.admin == true
+  end
+end
